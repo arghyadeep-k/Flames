bdc171a9e1d96971e53f25e1e1f6734aea0fe09e
Arghya <arghyadeep.kundu@gmail.com>
2018-08-12 00:33:30 +0530

    First Commit

---
+++ Flames/Predictor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flames
+{
+    class Predictor
+    {
+        private string name1;
+        private string name2;
+        private List<char> flames=new List<char>();
+        
+        private int count;
+        public Predictor(string name1, string name2)
+        {
+            this.name1 = name1.ToLower(); 
+            this.name2 = name2.ToLower(); 
+            this.count = name1.Length + name2.Length;
+            flames.Add('F');
+            flames.Add('L');
+            flames.Add('A');
+            flames.Add('M');
+            flames.Add('E');
+            flames.Add('S');
+        }
+
+        public string Compute()
+        {
+            char c;
+            for(int i = 0; i<name1.Length; i++)
+            {
+                c = name1[i];
+                //Console.Write(c);
+                int num = 0;
+                if (c != '#')
+                {
+                    num = NoOfOccurence(c, name2);
+                    count -= num;
+                    if (num > 0)
+                    {
+                        num = NoOfOccurence(c, name1);
+                        count -= num;
+                    }
+                    name1 = name1.Replace(c, '#');
+                }
+                
+            }
+            return FinalCount(count);
+        }
+
+        private string FinalCount(int count)
+        {
+            int index1 = 0;
+            int index2 = 0;
+
+            while (flames.Count>1)
+            {
+                if (index1 == (count - 1))
+                {
+                    flames.RemoveAt(index2);
+                    index1 = -1;
+                    index2--;
+                }
+                index1++;
+                index2++;
+                if (index2 >= flames.Count)
+                    index2 = 0;
+            }
+            char c = flames.ElementAt(0);
+
+            switch(c)
+            {
+                case 'F':
+                    return "Friends";
+                case 'L':
+                    return "Lovers";
+                case 'A':
+                    return "having an Affair";
+                case 'M':
+                    return "Marrying";
+                case 'E':
+                    return "Enemies";
+                case 'S':
+                    return "Siblings";
+                default:
+                    return "";
+            }
+        }
+
+        private int NoOfOccurence(char character,string name)
+        {
+            int occurence = 0;
+            foreach(char c in name)
+            {
+                if (c == character)
+                {
+                    occurence++;
+                }
+            }
+            return occurence;
+        }
+    }
+}
---
+++ Flames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1, name2, prediction = "";
+
+            Console.WriteLine("Enter 1st name: ");
+            name1 = Console.ReadLine();
+
+            Console.WriteLine("Enter 2nd name: ");
+            name2 = Console.ReadLine();
+
+            var predict = new Predictor(name1, name2);
+            prediction = predict.Compute();
+
+            Console.WriteLine("{0} and {1} will be {2}.", name1, name2,prediction);
+            Console.Read();
+        }        
+    }
+}
